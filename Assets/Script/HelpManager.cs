336f2f3a2f95654e0c0dc6724b10ffec17310ddc
SHFDev <sepehrhashemi57@gmail.com>
2025-06-04 17:18:09 +0330

    fix bug

---
+++ Assets/Script/HelpManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class HelpManager : MonoBehaviour
+{
+    public GameObject helpPanel;
+
+    void Start()
+    {
+        helpPanel.SetActive(false); // در شروع پنهان باشد
+    }
+
+    public void ShowHelp()
+    {
+        helpPanel.SetActive(true);
+    }
+
+    public void HideHelp()
+    {
+        helpPanel.SetActive(false);
+    }
+}
